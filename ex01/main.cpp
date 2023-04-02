3182373a60b770982c25bbab5e6ff89a439ca9ee
gbudau <54190400+gbudau@users.noreply.github.com>
2023-04-02 13:35:59 +0200

    Add ex01 RPN (#4)

---
+++ ex01/main.cpp
@@ -0,0 +1,17 @@
+#include "RPN.hpp"
+#include <iostream>
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    std::cout << "Error\n";
+    return 1;
+  }
+  try {
+    RPN rpn;
+    std::cout << rpn.calculate(argv[1]) << '\n';
+  } catch (const std::exception &) {
+    std::cout << "Error\n";
+    return 1;
+  }
+  return 0;
+}
